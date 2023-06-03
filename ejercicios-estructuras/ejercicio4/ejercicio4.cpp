f927c9f30667a37afeb0fdec950e7ca42f864710
VeroPi <36899173+VeroPi@users.noreply.github.com>
2023-06-02 21:51:51 -0300

    Update ejercicio4.cpp
    agregado de vector inscripciones

--- ejercicios-estructuras/ejercicio4/ejercicio4.cpp
+++ ejercicios-estructuras/ejercicio4/ejercicio4.cpp
@@ -38,12 +38,23 @@ int main () {
         cin >> boletas[i].legajo;
     }
 
+    Boleta inscripciones[i];
+
+    for(int j=0; j<i;j++){
+        inscripciones[j].legajo = boletas[j].legajo;
+        inscripciones[j].apellido = boletas[j].apellido;
+        inscripciones[j].codigoMateria = boletas[j].codigoMateria;
+        inscripciones[j].fecha.anio = boletas[j].fecha.anio;
+        inscripciones[j].fecha.mes = boletas[j].fecha.mes;
+        inscripciones[j].fecha.dia = boletas[j].fecha.dia;
+    }
+
     for (int j=0;j<i;j++) {
         cout << j << endl;
-        cout << "Legajo: " << boletas[j].legajo << endl;
-        cout << "Codigo de la materia: " << boletas[j].codigoMateria << endl;
-        cout << "Fecha del examen: " << boletas[j].fecha.dia << "/" << boletas[j].fecha.mes << "/" << boletas[j].fecha.anio << endl;
-        cout << "Apellido del alumno: " << boletas[j].apellido << endl;
+        cout << "Legajo: " << inscripciones[j].legajo << endl;
+        cout << "Codigo de la materia: " << inscripciones[j].codigoMateria << endl;
+        cout << "Fecha del examen: " << inscripciones[j].fecha.dia << "/" << inscripciones[j].fecha.mes << "/" << inscripciones[j].fecha.anio << endl;
+        cout << "Apellido del alumno: " << inscripciones[j].apellido << endl;
         cout << "---------------------" << endl;
     }
     
